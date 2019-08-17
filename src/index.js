01c99fa403b33568efbf5f9dbe7d59817aec490a
William Meleyal <william.meleyal@gmail.com>
2019-08-17 11:15:00 +0100

    Clean up exports

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
 import './banner'
-import * as compressor from './compressor'
 import * as constants from './constants'
-import * as music from './music'
-import * as number from './number'
 import * as part from './part'
 import * as phrase from './phrase'
-import * as reverb from './reverb'
-import * as sampler from './sampler'
-import * as samples from './samples'
 import * as score from './score'
 
 const gen = {
-  ...compressor,
   ...constants,
-  ...music,
-  ...number,
   ...part,
   ...phrase,
-  ...reverb,
-  ...sampler,
-  ...samples,
   ...score
 }
 
